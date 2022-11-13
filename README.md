e4444564488c82de19d5ce9f4c2abf6a83578f48
Aimery Tauveron <atauveron@via.ecp.fr>
2022-11-13 17:22:55 +0100

    Update bootstrap script

--- README.md
+++ README.md
@@ -2,28 +2,23 @@
 
 Configuration files and miscellaneous scripts for my Raspberry Pi (running Raspbian)
 
-
 ## .zshrc
 
 I use zsh with the [oh-my-zsh](http://ohmyz.sh/) framework and the [oh-my-via](https://github.com/badouralix/oh-my-via) theme.
-
 
 ## bootstrap.sh
 
 This bootstrap script installs a few useful packages, as well as the zsh shell, and the [oh-my-zsh](http://ohmyz.sh/) framework with the [oh-my-via](https://github.com/badouralix/oh-my-via) theme. It also deploys the `.zshrc` file automatically.
 
-
 ## dhcpcd.conf
 
 Configuration file to use a static IP address.
-
 
 ## iptables
 
 Simple IPv4 rules, for accepting connections from a local network (`192.168.0.0/16`) and dropping everything else. No IPv6 support (everything is dropped).
 
 Load the rules with `iptables-restore < rules.v4` and `ip6tables-restore < rules.v6`.
-
 
 ## fail2ban
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,6 @@ export DIR=/home/pi
 # Upgrade current installation
 echo
 echo "Upgrading current install..."
-echo
 # Update repositories
 sudo apt-get update
 # Upgrade packages
@@ -23,26 +22,22 @@ sudo apt-get dist-upgrade
 # Install new packages
 echo
 echo "Installing new packages..."
-echo
 # Basics
 sudo apt-get install vim git screen
 # Security
-sudo apt-get install fail2ban rkhunter
-# mosh (basically ssh over udp, handy when using a poor connection)
-sudo apt-get install mosh
+sudo apt-get install fail2ban iptables-persistent
 
 # Installing zsh, oh-my-zsh, and the oh-my-via theme
 echo
 echo "Installing zsh, installing and configuring oh-my-zsh..."
-echo
-apt-get install zsh
+sudo apt-get install zsh
+chsh $(which zsh)
 cp .zshrc $DIR
 git clone git://github.com/robbyrussell/oh-my-zsh.git $DIR/.oh-my-zsh
-export ZSH= "$DIR/.oh-my-zsh"
+export ZSH="$DIR/.oh-my-zsh"
 git clone https://github.com/badouralix/oh-my-via.git $ZSH/custom/themes/oh-my-via
 
 # Clean up
 echo
 echo "Cleaning the apt cache..."
-echo
 sudo apt-get clean
